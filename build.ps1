517d1740a18052b40b3ac53e892ef0dc3b49827a
Adam Rush <adamrushuk@gmail.com>
2017-05-05 20:15:26 +0100

    AVTest09

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Install-Module Psake, PSDeploy, BuildHelpers, VMware.PowerCLI -Force -AllowClobber
 Install-Module Pester -RequiredVersion 4.0.2 -Force -AllowClobber
-Import-Module Psake, BuildHelpers
+Import-Module Psake, PSDeploy, BuildHelpers, Pester, VMware.PowerCLI -Force
 
 Set-BuildEnvironment
 
