636978c892376aca54e8217e8dbe7c71b447fa3a
Adam Rush <arush@skyscapecloud.com>
2017-05-10 15:22:49 +0100

    Add example placeholder script

--- Examples/Export-CIEdge_Controller.ps1
+++ Examples/Export-CIEdge_Controller.ps1
@@ -31,20 +31,4 @@ param (
     $Path
 )
 
-<#
-Connect-CIServer -Server 'api.vcd.portal.skyscapecloud.com' -Org '1-1-1-a4f57h'
-
-Import-Module '.\PSvCloud\PSvCloud.psd1' -Force
-
-$CIEdgeView = Get-CIEdgeView -Name $Name
-$CIEdgeView
-
-$CIEdgeXML = $CIEdgeView | Get-CIEdgeXML
-$CIEdgeXML
-#>
-#$Name = 'nft001a4i2 -1'
-#$Name = 'nft001a4i2 -1', 'nft00191i2-1'
-#$Name = 'NameDoesNotExist'
-$Name = 'NameDoesNotExist', 'nft001a4i2 -1'
-$CIEdge = Get-CIEdge -Name $Name
-$CIEdge
+# TODO
