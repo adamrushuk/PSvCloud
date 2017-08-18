ef1ce6a645c5fd5428b75cce740fec09a7f98482
Adam Rush <arush@skyscapecloud.com>
2017-08-18 11:10:29 +0100

    Add ValidCPUMemoryMap

--- PSvCloud/Public/VM/Get-CIVMPrice.ps1
+++ PSvCloud/Public/VM/Get-CIVMPrice.ps1
@@ -14,6 +14,9 @@ function Get-CIVMPrice {
         A hashtable pricing matrix for different tiers, with the keys being a concatenation of CPU count and
         Memory in GB.
 
+        .PARAMETER ValidCPUMemoryMap
+        A hashtable to validate acceptable CPU / Memory configurations.
+
         .INPUTS
         System.Object
 
@@ -45,17 +48,28 @@ function Get-CIVMPrice {
         [ValidateNotNullOrEmpty()]
         [hashtable]
         $PricingMatrix = @{
-            10 = @{Size = 'Micro'; Essential = 0.01; Power = 0.02; Priority = 0.03}
-            12 = @{Size = 'Tiny'; Essential = 0.03; Power = 0.09; Priority = 0.135}
-            24 = @{Size = 'Small'; Essential = 0.04; Power = 0.12; Priority = 0.18}
-            48 = @{Size = 'Medium'; Essential = 0.06; Power = 0.22; Priority = 0.33}
-            416 = @{Size = 'Medium High Memory'; Essential = 0.14; Power = 0.35; Priority = 0.520}
-            816 = @{Size = 'Large'; Essential = 0.18; Power = 0.45; Priority = 0.675}
-            832 = @{Size = 'Large High Memory'; Essential = 0.35; Power = 0.55; Priority = 1.125}
-            848 = @{Size = 'Tier 1 Apps Small'; Essential = 0.50; Power = 0.60; Priority = 1.575}
-            864 = @{Size = 'Tier 1 Apps Medium'; Essential = 0.70; Power = 0.99; Priority = 2.085}
-            896 = @{Size = 'Tier 1 Apps Large'; Essential = 0.95; Power = 1.45; Priority = 2.675}
+            10    = @{Size = 'Micro'; Essential = 0.01; Power = 0.02; Priority = 0.03}
+            12    = @{Size = 'Tiny'; Essential = 0.03; Power = 0.09; Priority = 0.135}
+            24    = @{Size = 'Small'; Essential = 0.04; Power = 0.12; Priority = 0.18}
+            48    = @{Size = 'Medium'; Essential = 0.06; Power = 0.22; Priority = 0.33}
+            416   = @{Size = 'Medium High Memory'; Essential = 0.14; Power = 0.35; Priority = 0.520}
+            816   = @{Size = 'Large'; Essential = 0.18; Power = 0.45; Priority = 0.675}
+            832   = @{Size = 'Large High Memory'; Essential = 0.35; Power = 0.55; Priority = 1.125}
+            848   = @{Size = 'Tier 1 Apps Small'; Essential = 0.50; Power = 0.60; Priority = 1.575}
+            864   = @{Size = 'Tier 1 Apps Medium'; Essential = 0.70; Power = 0.99; Priority = 2.085}
+            896   = @{Size = 'Tier 1 Apps Large'; Essential = 0.95; Power = 1.45; Priority = 2.675}
             12128 = @{Size = 'Tier 1 Apps Extra Large'; Essential = 1.30; Power = 2.30; Priority = 'NA'}
+        },
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [hashtable]
+        $ValidCPUMemoryMap = @{
+            1  = @(0, 2)
+            2  = @(4)
+            4  = @(8, 16)
+            8  = @(16, 32, 48, 64, 96)
+            12 = @(128)
         }
     )
 
@@ -67,21 +81,43 @@ function Get-CIVMPrice {
 
             foreach ($VM in $CIVM) {
 
+                Write-Verbose "Processing $($VM.Name)"
+
+                # Round memory ready for key concatenation
                 $MemoryGBRounded = [math]::Round( $VM.MemoryGB )
-                $PricingMatrixKey = [int]"$($VM.CpuCount)$MemoryGBRounded"
+                Write-Verbose "`tMemoryGBRounded: $MemoryGBRounded"
+
+                # Original CPU Count
+                $OriginalCPUCount = $VM.CpuCount
+
+                # Get nearest CPU
+                $CPUNumberSet = $ValidCPUMemoryMap.Keys | Sort-Object
+                $ValidCPUCount = Get-EqualOrNextHighestNumber -Number $OriginalCPUCount -NumberSet $CPUNumberSet
+
+                # Get nearest Memory for CPU count
+                $MemoryNumberSet = $ValidCPUMemoryMap[$ValidCPUCount] | Sort-Object
+                $ValidMemoryCount = Get-EqualOrNextHighestNumber -Number $MemoryGBRounded -NumberSet $MemoryNumberSet
+
+                # Get nearest CPU / Memory config
+                $OriginalPricingMatrixKey = [int]"$($OriginalCPUCount)$MemoryGBRounded"
+                $NumberSet = $PricingMatrix.Keys | Sort-Object
+                $TempPricingMatrixKey = [int]"$($ValidCPUCount)$ValidMemoryCount"
+                $PricingMatrixKey = Get-EqualOrNextHighestNumber -Number $TempPricingMatrixKey -NumberSet $NumberSet
 
                 [PSCustomObject]@{
-                    Org       = $VM.Org
-                    OrgVdc    = $VM.OrgVdc
-                    vApp      = $VM.vApp
-                    Name      = $VM.Name
-                    Status    = $VM.Status
-                    CpuCount  = $VM.CpuCount
-                    MemoryGB  = $VM.MemoryGB
-                    Size      = $PricingMatrix[$PricingMatrixKey].Size
-                    Essential = $PricingMatrix[$PricingMatrixKey].Essential
-                    Power     = $PricingMatrix[$PricingMatrixKey].Power
-                    Priority  = $PricingMatrix[$PricingMatrixKey].Priority
+                    Org                  = $VM.Org
+                    OrgVdc               = $VM.OrgVdc
+                    vApp                 = $VM.vApp
+                    Name                 = $VM.Name
+                    Status               = $VM.Status
+                    CpuCount             = $OriginalCPUCount
+                    MemoryGB             = $VM.MemoryGB
+                    PricingMatrixKey     = $PricingMatrixKey
+                    Size                 = $PricingMatrix[$PricingMatrixKey].Size
+                    CustomConfigDetected = if ($OriginalPricingMatrixKey -eq $PricingMatrixKey) {$false} else {$true}
+                    Essential            = $PricingMatrix[$PricingMatrixKey].Essential
+                    Power                = $PricingMatrix[$PricingMatrixKey].Power
+                    Priority             = $PricingMatrix[$PricingMatrixKey].Priority
                 }
 
             }
